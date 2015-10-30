076a001118c69bac5df33a367d89fdcc77f21604
valiantvu <michelle.thi.vu@gmail.com>
2015-10-29 19:14:35 -0700

    Initial Commit

---
+++ HallOfFrames/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallOfFrames
+//
+//  Created by Michelle Vu on 10/29/15.
+//  Copyright (c) 2015 Michelle Vu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
